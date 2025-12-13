34a3a7e66366bd5457e4f8f2d463c0be02c33609
Ell Contrerax <sarmientodieg@gmail.com>
2025-12-13 09:52:22 -0500

    🐸◌*̥₊ AʟʏᴀBᴏᴛ - MD◌❐🌱༉

--- commands/downloader/instagram.js
+++ commands/downloader/instagram.js
@@ -5,15 +5,6 @@ export default {
   category: 'downloader',
   run: async (client, m, args, command) => {
 
-    const botId = client.user.id.split(':')[0] + '@s.whatsapp.net'
-    const isOficialBot = botId === global.client.user.id.split(':')[0] + '@s.whatsapp.net'
-    const isPremiumBot = global.db.data.settings[botId]?.botprem === true
-    const isModBot = global.db.data.settings[botId]?.botmod === true
-
-    if (!isOficialBot && !isPremiumBot && !isModBot) {
-      return client.reply(m.chat, `《✧》El comando *${command}* no está disponible en *Sub-Bots.*`, m)
-    }
-
     const url = args[0]
 
     if (!url) {
