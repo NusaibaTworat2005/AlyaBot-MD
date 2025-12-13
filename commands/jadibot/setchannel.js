376fdcc5ec20e4c02fe3003bc9d75b1fa5a99af8
Ell Contrerax <sarmientodieg@gmail.com>
2025-12-13 10:22:56 -0500

    Crear setchannel.js

---
+++ commands/jadibot/setchannel.js
@@ -0,0 +1,29 @@
+export default {
+  command: ['setchannel', 'setbotchannel'],
+  category: 'socket',
+  run: async (client, m, args) => {
+    const idBot = client.user.id.split(':')[0] + '@s.whatsapp.net'
+    const config = global.db.data.settings[idBot]
+    const isOwner2 = [idBot, ...global.owner.map((number) => number + '@s.whatsapp.net')].includes(m.sender)
+    if (!isOwner2 && m.sender !== owner) return m.reply(mess.socket)
+    const value = args.join(' ').trim()
+    if (!value) {
+      return m.reply(`❀ Ingresa el enlace o el ID de un canal de WhatsApp.\n\nEjemplos:\n*${client.prefix}setchannel* https://whatsapp.com/channel/XXXXXXXXXXXXXX\n*${client.prefix}setchannel* 12345@newsletter`)
+    }
+    let info, ch
+    if (/@newsletter$/i.test(value)) {
+      ch = value.trim()
+      info = await client.newsletterMetadata("jid", ch)
+    } else {
+      const channelUrl = value.match(/(?:https:\/\/)?(?:www\.)?(?:chat\.|wa\.)?whatsapp\.com\/(?:channel\/|joinchat\/)?([0-9A-Za-z]{22,24})/i)?.[1]
+      if (!channelUrl) return m.reply('ꕥ El enlace o ID proporcionado no es válido.')
+      info = await client.newsletterMetadata("invite", channelUrl)
+      ch = info?.id
+    }
+    if (!info) return m.reply('ꕥ No se pudo obtener información del canal.')
+    config.id = info.id
+    config.nameid = info.thread_metadata.name.text || "Canal sin nombre"
+// m.reply(JSON.stringify(info, null, 2))
+    return m.reply(`❀ Se cambió el canal del Socket a *"${config.nameid}"* correctamente.`)
+  },
+};
