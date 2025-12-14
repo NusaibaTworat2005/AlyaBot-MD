dd41211622fbebfd6e77cfe82f73b5547bdc5f11
Ell Contrerax <sarmientodieg@gmail.com>
2025-12-14 12:32:30 -0500

    🐸◌*̥₊ AʟʏᴀBᴏᴛ - MD◌❐🌱༉

--- index.js
+++ index.js
@@ -127,8 +127,7 @@ align: 'center',
 gradient: ['blue', 'magenta']
 })
 
-const message = `${subtitleStyle('Desarrollado por »')} ${responseStyle(author.name)}
-${subtitleStyle('Código basado por »')} ${responseStyle(collaborators.col1)}
+const message = `${subtitleStyle('Desarrollado por »')} ${responseStyle(author)}
 ${subtitleStyle('Colaboradores activos »')} ${responseStyle(activeCollaborators)}
 ${subtitleStyle('Versión »')} ${responseStyle(version)}`
 console.log(boxen(message, { padding: 1, margin: 1, borderStyle: 'double', borderColor: 'blue', float: 'center', }))
