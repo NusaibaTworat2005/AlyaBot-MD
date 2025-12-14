064de7f42de31890c6f98546c050c070c6b20f69
Ell Contrerax <sarmientodieg@gmail.com>
2025-12-14 12:07:35 -0500

    🐸◌*̥₊ AʟʏᴀBᴏᴛ - MD◌❐🌱༉
    🐸◌*̥₊ AʟʏᴀBᴏᴛ - MD◌❐🌱༉

--- commands/owner/exec2.js
+++ commands/owner/exec2.js
@@ -1,13 +1,14 @@
 import cp from 'child_process'
 import { promisify } from 'util'
 const exec = promisify(cp.exec)
+
 export default {
   command: ['r'],
   isOwner: true,
   run: async (client, m, args) => {
     const cmd = args.join(' ').trim()
     if (!cmd) {
-      return client.reply(m.chat, '❌ Escribe un comando para ejecutar.\nEjemplo: .r ls', m)
+      return // client.reply(m.chat, '❌ Escribe un comando para ejecutar.\nEjemplo: .r ls', m)
     }
     let result
     try {
