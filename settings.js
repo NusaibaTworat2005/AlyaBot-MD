930985a56b7f79ab9f10b592b252a4e2635470db
Ell Sarmiento 🦩 <sarmientodieg@gmail.com>
2025-12-24 19:25:47 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -23,7 +23,7 @@ global.pairing_code = true
 global.number_bot = ''
 global.msgglobal = '[Error: *TypeError*] fetch failed'
 global.dev = '☄︎ ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴛᴇʟʟᴀʀ'
-global.termux = false
+global.termux = true
 
 global.api = {
   url: 'https://api.stellarwa.xyz',
